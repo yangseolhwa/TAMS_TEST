963075881357cb1f47743557348cb1b2a0473e8b
9hakboiii <gyuhakarmy@gmail.com>
2026-03-17 17:55:51 +0900

    feat: logout, refresh api 구현

--- back/routes/authRoutes.js
+++ back/routes/authRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authControllers');
+const { verifyAccessToken } = require('../middleware/authMiddleware');
 
 //  POST /api/auth/login
 router.post('/login', authController.login);
+
+// POST /api/auth/logout
+router.post('/logout', verifyAccessToken, authController.logout);
+
+// POST /api/auth/refresh
+router.post('/refresh', authController.refresh);
+
 
 module.exports = router;
