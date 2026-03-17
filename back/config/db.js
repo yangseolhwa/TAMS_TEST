e9a861b71306436c7562905ca9363e5772e2439f
9hakboiii <gyuhakarmy@gmail.com>
2026-03-17 15:12:50 +0900

    fix: port에서 DB_USER로 설정되어 있는 부분 수정

--- back/config/db.js
+++ back/config/db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_USER,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: process.env.NODE_ENV === 'production' ? false : console.log, // 개발 중일 떄만 사용
     timezone: '+09:00'
