cbdae74b3b471dfdc2cc7596ab5980ef67b9f2e7
9hakboiii <gyuhakarmy@gmail.com>
2026-03-17 17:58:23 +0900

    chore: 줄바꿈 제거

--- back/routes/authRoutes.js
+++ back/routes/authRoutes.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authControllers');
-const { verifyAccessToken } = require('../middleware/authMiddleware');
+const authController = require("../controllers/authControllers");
+const { verifyAccessToken } = require("../middleware/authMiddleware");
 
 //  POST /api/auth/login
-router.post('/login', authController.login);
+router.post("/login", authController.login);
 
 // POST /api/auth/logout
-router.post('/logout', verifyAccessToken, authController.logout);
+router.post("/logout", verifyAccessToken, authController.logout);
 
 // POST /api/auth/refresh
-router.post('/refresh', authController.refresh);
-
+router.post("/refresh", authController.refresh);
 
 module.exports = router;
