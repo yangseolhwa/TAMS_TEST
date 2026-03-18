09f059f309df58bc24ad083b62bdd6f41c2e03a1
yangseolhwa <yangse879@gmail.com>
2026-03-18 17:14:54 +0900

    feat: 기본 레이아웃 구성

---
+++ front/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,33 @@
+import styles from './DefaultLayout.module.css'
+
+const DefaultLayout = ({ children }) => {
+  return (
+    <div className={styles.wrapper}>
+
+      {/* Header */}
+      <header className={styles.header}>
+        <div className={styles.headerLogo}>
+          {/* 로고 추가 예정*/}
+        </div>
+        <div className={styles.headerRight}>
+          {/* 유저 정보 + 로그아웃 추가 예정 */}
+        </div>
+      </header>
+
+      {/* Navigation Tab Bar */}
+      <nav className={styles.nav}>
+        <div className={styles.navTabs}>
+          {/* 탭 추가 예정 */}
+        </div>
+      </nav>
+
+      {/* Main Content */}
+      <main className={styles.main}>
+        {children}
+      </main>
+
+    </div>
+  )
+}
+
+export default DefaultLayout
