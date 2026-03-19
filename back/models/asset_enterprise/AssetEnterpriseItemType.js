19ef80d0c9710cf6b7e96a50bf9175dfb78f0a6b
9hakboiii <gyuhakarmy@gmail.com>
2026-03-19 10:29:13 +0900

    fix: 개인 자산 대분류 필드 null 허용

--- back/models/asset_enterprise/AssetEnterpriseItemType.js
+++ back/models/asset_enterprise/AssetEnterpriseItemType.js
@@ -3,7 +3,7 @@ const sequelize = require('../../config/db');
 
 const AssetEnterpriseItemType = sequelize.define('AssetEnterpriseItemType', {
   id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-  category_id: {type: DataTypes.INTEGER},
+  category_id: {type: DataTypes.INTEGER, allowNull: false},
   name: {type: DataTypes.STRING(50)},
 }, {
   tableName: 'asset_enterprise_item_type',
