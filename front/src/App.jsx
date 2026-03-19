6d7849cc12ab8a7b980f4d483ef4d72ca1fdfe17
yangseolhwa <yangse879@gmail.com>
2026-03-19 15:48:35 +0900

    feat: 로그인 관련 라우팅 설정

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,50 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useState } from 'react'
 import LoginPage from './components/Login/LoginPage'
 import DefaultLayout from './layouts/DefaultLayout/DefaultLayout'
 
 function App() {
-  const [role, setRole] = useState(null)  // null이면 로그인 화면
+  const [role, setRole] =  useState(sessionStorage.getItem('role'))
 
-  if (!role) {
-    return <LoginPage onLoginSuccess={setRole} />
+  const handleLoginSuccess = (role) => {
+    sessionStorage.setItem('role', role)  // ← 추가
+    setRole(role)
   }
 
+  const handleLogout = () => {
+    sessionStorage.removeItem('role')  // ← 추가
+    setRole(null)
+  }
+  
   return (
-    <DefaultLayout role={role} onLogout={() => setRole(null)}>
-      <p>레이아웃 확인용</p>
-    </DefaultLayout>
+    <BrowserRouter>
+      <Routes>
+        {/* 로그인 */}
+        <Route
+          path="/login"
+          element={
+            role
+              ? <Navigate to={`/${role}/my-assets`} replace />
+              : <LoginPage onLoginSuccess={handleLoginSuccess} />
+          }
+        />
+
+        {/* 메인 */}
+        <Route
+          path="/:role/*"
+          element={
+            role
+              ? <DefaultLayout role={role} onLogout={handleLogout}>
+                  <p>레이아웃 확인용</p>
+                </DefaultLayout>
+              : <Navigate to="/login" replace />
+          }
+        />
+
+        {/* 기본 리다이렉트 */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
