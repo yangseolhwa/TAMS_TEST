8ea7384abc057a04af59d61b3f5f92a62a5edd70
9hakboiii <gyuhakarmy@gmail.com>
2026-03-19 10:34:33 +0900

    chore: 공백 제거

--- back/models/index.js
+++ back/models/index.js
@@ -1,16 +1,16 @@
 const User = require('./user_auth/User');
 const RefreshToken = require('./user_auth/RefreshToken');
-const AssetEnterprise         = require('./asset_enterprise/AssetEnterprise');
+const AssetEnterprise = require('./asset_enterprise/AssetEnterprise');
 const AssetEnterpriseCategory = require('./asset_enterprise/AssetEnterpriseCategory');
 const AssetEnterpriseItemType = require('./asset_enterprise/AssetEnterpriseItemType');
-const AssetEnterpriseRequest  = require('./asset_enterprise/AssetEnterpriseRequest');
-const AssetSw                 = require('./asset_sw/AssetSw');
-const AssetSwLicense          = require('./asset_sw/AssetSwLicense');
-const AssetSwRequest          = require('./asset_sw/AssetSwRequest');
-const AssetProject            = require('./asset_project/AssetProject');
-const AssetProjectItem        = require('./asset_project/AssetProjectItem');
-const AssetProjectItemType    = require('./asset_project/AssetProjectItemType');
-const AssetProjectHistory     = require('./asset_project/AssetProjectHistory');
+const AssetEnterpriseRequest = require('./asset_enterprise/AssetEnterpriseRequest');
+const AssetSw = require('./asset_sw/AssetSw');
+const AssetSwLicense = require('./asset_sw/AssetSwLicense');
+const AssetSwRequest = require('./asset_sw/AssetSwRequest');
+const AssetProject = require('./asset_project/AssetProject');
+const AssetProjectItem = require('./asset_project/AssetProjectItem');
+const AssetProjectItemType = require('./asset_project/AssetProjectItemType');
+const AssetProjectHistory = require('./asset_project/AssetProjectHistory');
 
 // ── User 관계 ──
 User.hasMany(RefreshToken, { foreignKey: 'user_id' });
@@ -29,11 +29,11 @@ AssetProject.belongsTo(User, { foreignKey: 'user_id' });
 AssetEnterprise.belongsTo(AssetEnterpriseCategory, { foreignKey: 'category_id',  as: 'category' });
 AssetEnterprise.belongsTo(AssetEnterpriseItemType, { foreignKey: 'item_type_id', as: 'itemType' });
 
-AssetEnterpriseCategory.hasMany(AssetEnterpriseItemType, { foreignKey: 'category_id' });
-AssetEnterpriseItemType.belongsTo(AssetEnterpriseCategory, { foreignKey: 'category_id' });
+AssetEnterpriseCategory.hasMany(AssetEnterpriseItemType, { foreignKey: 'category_id', as: 'itemTypes' });
+AssetEnterpriseItemType.belongsTo(AssetEnterpriseCategory, { foreignKey: 'category_id', as: 'category' });
 
-AssetEnterprise.hasMany(AssetEnterpriseRequest, { foreignKey: 'asset_id',      as: 'requests' });
-AssetEnterpriseRequest.belongsTo(AssetEnterprise, { foreignKey: 'asset_id',      as: 'asset' });
+AssetEnterprise.hasMany(AssetEnterpriseRequest, { foreignKey: 'asset_id', as: 'requests' });
+AssetEnterpriseRequest.belongsTo(AssetEnterprise, { foreignKey: 'asset_id', as: 'asset' });
 AssetEnterpriseRequest.belongsTo(User, { foreignKey: 'requester_id',  as: 'requester' });
 
 // ── AssetSw 관계 ──
@@ -53,8 +53,8 @@ AssetProjectItemType.hasMany(AssetProjectItem, { foreignKey: 'asset_type_id' });
 
 AssetProjectItem.hasMany(AssetProjectHistory, { foreignKey: 'asset_project_item_id', as: 'histories' });
 AssetProjectHistory.belongsTo(AssetProjectItem, { foreignKey: 'asset_project_item_id', as: 'item' });
-AssetProjectHistory.belongsTo(AssetProject, { foreignKey: 'project_id',            as: 'project' });
-AssetProjectHistory.belongsTo(User, { foreignKey: 'change_by',             as: 'changedBy' });
+AssetProjectHistory.belongsTo(AssetProject, { foreignKey: 'project_id', as: 'project' });
+AssetProjectHistory.belongsTo(User, { foreignKey: 'change_by', as: 'changedBy' });
 
 module.exports = {
   User, RefreshToken,
