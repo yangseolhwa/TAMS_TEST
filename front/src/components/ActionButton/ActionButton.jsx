60942c0a7475f16ed858a66126e386d65d1ee17b
yangseolhwa <yangse879@gmail.com>
2026-04-02 21:12:29 +0900

    feat: 공통 버튼(파랑, 빨강, 검정, 흰) 컴포넌트 추가

---
+++ front/src/components/ActionButton/ActionButton.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import styles from './ActionButton.module.css';
+
+const ActionButton = ({ label, onClick, variant = 'blue', size = 'sm', disabled = false, ...props }) => {
+  return (
+    <button
+      type="button"
+      className={`${styles.btn} ${styles[variant]} ${styles[size]}`}
+      onClick={onClick}
+      disabled={disabled}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+ActionButton.propTypes = {
+  label:    PropTypes.string.isRequired,
+  onClick:  PropTypes.func.isRequired,
+  variant:  PropTypes.oneOf(['blue', 'red', 'white', 'black']),
+  size:     PropTypes.oneOf(['sm', 'md']),
+  disabled: PropTypes.bool,
+};
+
+export default ActionButton;
