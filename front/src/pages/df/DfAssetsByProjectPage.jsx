f790407dd25013a3a64a9f126c4646022516098e
yangseolhwa <yangse879@gmail.com>
2026-04-07 17:01:01 +0900

    feat: DF 자산 관리 메인 화면 구성

---
+++ front/src/pages/df/DfAssetsByProjectPage.jsx
@@ -0,0 +1,5 @@
+const DfAssetsByProjectPage = () => {
+  return <div>프로젝트별 상세 조회 (준비 중)</div>
+}
+
+export default DfAssetsByProjectPage
---
+++ front/src/pages/df/DfAssetsRegisterPage.jsx
@@ -0,0 +1,5 @@
+const DfAssetsRegisterPage = () => {
+  return <div>DF 자산 등록 (준비 중)</div>
+}
+
+export default DfAssetsRegisterPage
