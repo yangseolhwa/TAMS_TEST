6c3599e67104442c10685af623f7bb0006cd55a6
9hakboiii <gyuhakarmy@gmail.com>
2026-04-10 16:48:17 +0900

    feat: 대시보드, sw/pc 조회 전용 API 구현

--- back/routes/assetRoutes.js
+++ back/routes/assetRoutes.js
@@ -16,30 +16,33 @@ router.use(verifyAccessToken);
 
 // 조회
 router.get('/personal', assetController.getPersonalAssets);
-router.get('/df',       assetController.getDfAssets);
+router.get('/dashboard', assetController.getSwList);
+router.get('/sw', assetController.getSwList);
+router.get('/enterprise', assetController.getEnterpriseList);
+router.get('/df', assetController.getDfAssets);
 
 // 등록 요청 목록 조회
 router.get('/requests', assetController.getRequests);
 
 // 등록
 router.post('/enterprise', assetController.registerEnterprise);
-router.post('/sw',         assetController.registerSw);
-router.post('/df',         assetController.registerDf);
+router.post('/sw', assetController.registerSw);
+router.post('/df', assetController.registerDf);
 
 // 관리자 승인 / 반려
 router.patch('/enterprise/approve/:requestId', assetController.approveEnterprise);
 router.patch('/enterprise/reject/:requestId',  assetController.rejectEnterprise);
-router.patch('/sw/approve/:requestId',         assetController.approveSw);
-router.patch('/sw/reject/:requestId',          assetController.rejectSw);
+router.patch('/sw/approve/:requestId', assetController.approveSw);
+router.patch('/sw/reject/:requestId', assetController.rejectSw);
 
 // 반납
 router.patch('/enterprise/return', assetController.returnEnterprise);
-router.patch('/sw/return',         assetController.returnSw);
-router.patch('/df/return',         assetController.returnDf);
+router.patch('/sw/return', assetController.returnSw);
+router.patch('/df/return', assetController.returnDf);
 
 // 이동
 router.patch('/enterprise/move', assetController.moveEnterprise);
-router.patch('/df/move',         assetController.moveDf);
+router.patch('/df/move', assetController.moveDf);
 
 // DF 엑셀 import / export / 양식 다운로드
 router.post('/df/import', upload.single('file'), importDf);
